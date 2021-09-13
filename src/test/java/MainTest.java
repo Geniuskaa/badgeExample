c0f58817f6c4feb1744b8c9d53eb20930265ed97
Geniuskaa <eminizinar@gmail.com>
2021-09-13 12:35:22 +0300

    Initial commit

---
+++ src/test/java/MainTest.java
@@ -0,0 +1,12 @@
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class MainTest {
+
+    @Test
+    public void testSum() {
+        assertEquals(10, Main.sum(4,6));
+    }
+}
